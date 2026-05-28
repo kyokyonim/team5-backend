681d07fa2ee88ff35154228aa2ecce6e2672a867
jay <jay80790@gmail.com>
2026-05-29 02:13:01 +0900

    test(chat): add h2 test profile for spring boot context test stability

--- src/test/java/com/team5/web_ide/WebIdeApplicationTests.java
+++ src/test/java/com/team5/web_ide/WebIdeApplicationTests.java
@@ -2,8 +2,10 @@ package com.team5.web_ide;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebIdeApplicationTests {
 
 	@Test
