6dce717ef86215d3053ca6de1045baeaa579a10e
다은김 <daeun-kim@da-eungim-ui-noteubug.local>
2026-05-31 16:26:22 +0900

    feat: 구글 소셜 로그인 구현

--- src/main/java/com/team5/web_ide/domain/user/repository/UserRepository.java
+++ src/main/java/com/team5/web_ide/domain/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.team5.web_ide.domain.user.repository;
-import com.team5.web_ide.domain.user.entity.User;
 
+import com.team5.web_ide.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
+    boolean existsByEmailAndProvider(String email, User.Provider provider);
+    Optional<User> findByEmailAndProvider(String email, User.Provider provider);
 }
