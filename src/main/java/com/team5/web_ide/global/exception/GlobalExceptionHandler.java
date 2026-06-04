7ac1dc96a4659f90b3af53640856f6a5cecbd8b0
jeong-yunseo <jyunseo432@gmail.com>
2026-06-05 00:45:15 +0900

    fix: redirect google oauth to frontend callback

--- src/main/java/com/team5/web_ide/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/team5/web_ide/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.team5.web_ide.global.exception;
 
 import com.team5.web_ide.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -40,8 +43,17 @@ public class GlobalExceptionHandler {
                 ));
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Void>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.fail("BAD_REQUEST", e.getMessage()));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        log.error("Unhandled server exception", e);
+
         return ResponseEntity
                 .status(GlobalErrorCode.INTERNAL_SERVER_ERROR.getStatus())
                 .body(ApiResponse.fail(
